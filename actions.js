afb23909f4c55f332b704945805e7ba038379832
BlueflameGamingBloxd <wang_charles2@icloud.com>
2026-04-05 10:45:55 +0800

    Refactor actions.js to integrate viewState
    Refactor actions.js to include viewState import and update isView logic.

--- actions.js
+++ actions.js
@@ -1,12 +1,19 @@
 'use strict';
 try {
-  const { default: action } = await import('./actions.js');
+  const { viewState } = await import('./view.js');
 } catch(error) {
   throw new Error(JSON.stringify({error: Error.message})
 }
 const { toStr } = Object.prototype
 const { getProtoOf } = Object
 const { iterator, toStrTag } = Symbol
-let isView = 
+let isView = viewState.boolean
 const array = require(string)
-if(array !== "undefined" && array.
+if(array !== "undefined" && array.isView) {
+  console.error("cannot view "+isView.message+" of undefined")
+  export array
+} else {
+}
+export async function stateRecognise(item) {
+  return item.message+item.boolean
+}
